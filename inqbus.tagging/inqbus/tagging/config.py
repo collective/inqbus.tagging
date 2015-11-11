14c86478572c663d17c3a06ac44993c9c38bace6
Sandra Rum <sandra.rum@inqbus.de>
2015-11-11 15:09:23 +0100

    Initial commit.

---
+++ inqbus.tagging/inqbus/tagging/config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Configuration options for inqbus.tagging"""
+
+PROJECT_NAME = 'inqbus.tagging'
+INSTALL_PROFILE = '{0}:default'.format(PROJECT_NAME)
+UNINSTALL_PROFILE = '{0}:uninstall'.format(PROJECT_NAME)
