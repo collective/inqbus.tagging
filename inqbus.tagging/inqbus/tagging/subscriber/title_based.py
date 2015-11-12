43afad1aecae7bff51311fc10223e9e8897c1205
Sandra Rum <sandra.rum@inqbus.de>
2015-11-12 15:42:32 +0100

    Added tags depending on file-title.

---
+++ inqbus.tagging/inqbus/tagging/subscriber/title_based.py
@@ -0,0 +1,29 @@
+
+def title_to_tag(context, event):
+    image = context.image
+
+    filename = image.filename
+    filename = filename.replace(' ', '.').replace('_', '.').replace('/', '.')
+
+    name_tags = filename.split('.')
+
+    name_tags.pop(-1)
+
+    tags = list(context.Subject()) + name_tags
+
+    tags = list(set(tags))
+
+    clear_tags = []
+
+    for tag in tags:
+        try:
+            int(tag)
+        except ValueError:
+            clear_tags.append(tag)
+        else:
+            continue
+
+    print clear_tags
+
+    context.setSubject(clear_tags)
+    context.reindexObject()
